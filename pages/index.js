1ad22cac38129b1fbf4b899267198b6e126dbe05
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-13 23:19:47 -0500

    Update index.js
    The Book Of title

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
      
       <main className={styles.main}>
         <h1 className={styles.title}>
-         <a href="https://nextjs.org">Romans</a>
+           <a href="https://nextjs.org">The Book Of</a>
         </h1>
          <div className={styles.hero}>
        <Image 
